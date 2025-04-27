21d4f3cf6aa33336ba61cb146739dd4533eaf2ee
RodGonzalez <185538443+UliRodGonzZa@users.noreply.github.com>
2025-04-26 23:03:58 -0600

    [RODRIGO][MAIN][UPDATE] Update fecha.sh

--- comandos/fecha.sh
+++ comandos/fecha.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
-
-# Script fecha.sh
-# Muestra la fecha y hora del sistema, ajustada al huso horario de CDMX, con arte ASCII.
-
 mostrar_fecha() {
     clear
 
-    # Mostrar título
+    # Mostrar título estilizado
     echo ""
     toilet -f smblock "Fecha y Hora del Sistema" | boxes -d stone | lolcat -a -d 2
     echo ""
 
-    # Obtener fecha y hora
+    # Obtener hora
     if [ -f /proc/driver/rtc ]; then
+         # Si existe el archivo /proc/driver/rtc (controlador del reloj de tiempo real, RTC):
         fecha=$(grep "rtc_date" /proc/driver/rtc | sed 's/rtc_date[ \t]*:[ \t]*//')
+        # Extrae la fecha (rtc_date) eliminando etiquetas y espacios innecesarios.
         hora=$(grep "rtc_time" /proc/driver/rtc | sed 's/rtc_time[ \t]*:[ \t]*//')
 
         IFS='-' read -r anio mes dia <<< "$fecha"
+          # Divide la fecha obtenida en año, mes y día usando el carácter '-' como separador.
         IFS=':' read -r h m s <<< "$hora"
-
+        # Corrige la hora del sistema para ajustarla al huso horario de la CDMX
+        # restando 6 horas. Se usa (10#$h) para tratar la hora como decimal puro.
         h_corrigida=$(( (10#$h - 6 + 24) % 24 ))
         hora_mexico=$(printf "%02d:%02d:%02d" "$h_corrigida" "$m" "$s")
     else
+           # Si no existe el archivo RTC utilizamos comando basico =>
         fecha_hora=$(date "+%d/%m/%Y %H:%M:%S")
         hora_mexico="$fecha_hora"
         h_corrigida=$(date "+%H")
@@ -31,40 +31,62 @@ mostrar_fecha() {
 
     echo ""
     cat << EOF | lolcat -a -d 2
-Hora del Sistema: $hora_sistema
-Hora Ciudad de México: $hora_mexico
+    Hora del Sistema: $hora_sistema
+    Hora Ciudad de México: $hora_mexico
 EOF
     echo ""
 
-    # Mostrar imagen dependiendo de hora
+    # Mostrar el dibujo ASCII
     if [ "$h_corrigida" -ge 6 ] && [ "$h_corrigida" -lt 18 ]; then
         # Día
         cat << "EOF" | lolcat -a -d 2
-            .   
-        .   :   .  
-    '.   .  :  .   .'
- ._   '._.-'''-._.'   _.
-   '-..'         '..-' 
---._ /.==.     .==.\ _.--
-    ;/_o__\   /_o__\;
-----|`     ) (     `|----
-   _: \_) (\_/) (_/ ;_
---'  \  '._.=._.'  /  '-- 
-  _.-'''.  '._.'  .'''-._
- '    .''-.(_).-.''    '
-      .'   '  :  '   '.
-         '    :   '
+             .
+         .   :   .  
+     '.   .  :  .   .'
+  ._   '._.-'''-._.'   _.
+    '-..'         '..-' 
+ --._ /.==.     .==.\ _.--
+     ;/_o__\   /_o__\;
+-----|`     ) (     `|-----
+    _: \_) (\_/) (_/ ;_
+ --'  \  '._.=._.'  /  '--
+   _.-''.  '._.'  .''-._
+  '    .''-.(_).-''.    '
+     .'   '  :  '   '.
+        '    :   '
 EOF
     else
         # Noche
         cat << "EOF" | lolcat -a -d 2
-(Noche: Imagen de estrellas muy grande)
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣤⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠻⠿⢿⣿⣿⣿⣿⣿⣶⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣙⢿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡟⠹⠿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠋⡬⢿⣿⣷⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡇⢸⡇⢸⣿⣿⣿⠟⠁⢀⣬⢽⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣧⣈⣛⣿⣿⣿⡇⠀⠀⣾⠁⢀⢻⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣧⣄⣀⠙⠷⢋⣼⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
+⠸⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀
+⠀⢹⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀
+⠀⠀⠹⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀
+⠀⠀⠀⠙⣿⣿⣿⣿⣿⣶⣤⣀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠅⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠈⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠛⠛⠛⠛⠛⠋⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 EOF
     fi
 
     echo ""
     read -p "Presiona ENTER para volver al menú..."
 }
-
-# Ejecutar función
 mostrar_fecha
+clear
